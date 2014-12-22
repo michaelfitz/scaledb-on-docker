aa42f5d474037d36bf3ca4c9e01daf417022c259
Ubuntu <michael@scaledock1.scaledock1.h5.internal.cloudapp.net>
2014-12-22 05:20:05 +0000

    rename primary

--- deploy_cas.sh
+++ deploy_cas.sh
@@ -2,10 +2,10 @@
 
 sudo docker build -t="michaelfitz/myscaledb:106" CAS/.
 
-sudo docker rm -f cas11 
+sudo docker rm -f casp
 
 
-SID1=`sudo docker run -t  -d -i --name cas11  -p 4306:4306 --env-file=deploy_cas.env   michaelfitz/myscaledb:106`
+SID1=`sudo docker run -t  -d -i --name casp  -p 4306:4306 --env-file=deploy_cas.env   michaelfitz/myscaledb:106`
 
 
 
