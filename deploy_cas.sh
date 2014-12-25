284e1e10f85cd5c091fd8b51f6bf677f841e7d60
Ubuntu <michael@scaledock8.scaledock8.h3.internal.cloudapp.net>
2014-12-25 19:35:21 +0000

    add additional CAS nodes

--- deploy_cas.sh
+++ deploy_cas.sh
@@ -5,7 +5,7 @@ sudo docker build -t="michaelfitz/myscaledb:106" CAS/.
 sudo docker rm -f casp
 
 
-SID1=`sudo docker run -t  -d -i --name casp  -p 4306:4306 --env-file=deploy_cas.env   michaelfitz/myscaledb:106`
+SID1=`sudo docker run -t  -d -i --name cas1  -p 4306:4306    michaelfitz/myscaledb:106`
 
 
 
