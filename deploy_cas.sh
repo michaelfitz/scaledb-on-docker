f2712700c040b9c225d4f54d6eb2742f6b42fc40
Ubuntu <michael@scaledock8.scaledock8.h3.internal.cloudapp.net>
2014-12-25 20:59:22 +0000

    fix type

--- deploy_cas.sh
+++ deploy_cas.sh
@@ -2,7 +2,7 @@
 
 sudo docker build -t="michaelfitz/myscaledb:106" CAS/.
 
-sudo docker rm -f casp
+sudo docker rm -f cas1
 
 
 SID1=`sudo docker run -t  -d -i --name cas1  -p 4306:4306    michaelfitz/myscaledb:106`
