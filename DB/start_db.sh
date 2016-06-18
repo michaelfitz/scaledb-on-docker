114d819dfab83e1cfe5954e36b4169345b99ea81
michaelfitz <michael@scaledb.com>
2016-06-19 07:43:20 +1000

    Update start_db.sh

--- DB/start_db.sh
+++ DB/start_db.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mkdir scaledb/data
-mkdir scaledb/logs
+##mkdir scaledb/data
+##mkdir scaledb/logs
 
-export LD_LIBRARY_PATH=/scaledb-0.2.5/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=/usr/local/scaledb/lib#:${LD_LIBRARY_PATH}
 
 
 
@@ -20,15 +20,17 @@ echo "CASP_ADDRESS= "$CASP_PORT_4306_TCP_ADDR
 sed -e "s|OPENSHIFT_MARIADB_DB_HOST|${CAS_IP}|" my.cnf.1  > my.cnf
 sed -e "s|OPENSHIFT_MARIADB_DB_HOST|${CASP_PORT_4306_TCP_ADDR}|" scaledb.cnf.1  > scaledb.cnf
 mv scaledb.cnf /mariadb-10.0.12-linux-x86_64/data
+mv scaledb.cnf /usr/local/mariadb/data
 
 
 #sed -e "s|CASP_PORT_4306_TCP_PORT|${CASP_PORT_4306_TCP_PORT}|" slm.cnf.2  > slm.cnf.3
 #sed -e "s|CASP_PORT_4306_TCP_ADDR|${CASP_PORT_4306_TCP_ADDR}|" slm.cnf.3  > slm.cnf
 
-cp /scaledb-0.2.5/lib/ha_scaledb.so /mariadb-10.0.12-linux-x86_64/lib/plugin
+
+cp /usr/local/scaledb/lib/ha_scaledb.so /usr/local/mariadb/lib/plugin
 
 
-/mariadb-10.0.12-linux-x86_64/scripts/mysql_install_db  --basedir=/mariadb-10.0.12-linux-x86_64 --defaults-file=/my.cnf
+/usr/local/mariadb/scripts/mysql_install_db  --basedir=/usr/local/mariadb --defaults-file=/my.cnf
 /mariadb-10.0.12-linux-x86_64/bin/mysqld --defaults-file=/my.cnf  --user=root &
 sleep 10
 /mariadb-10.0.12-linux-x86_64/bin/mysql  -uroot -e "INSTALL PLUGIN scaledb SONAME 'ha_scaledb.so'"
