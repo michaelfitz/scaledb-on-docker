135711a3b21b272c0974a8b7c8d00be334edfe17
michaelfitz <michael@scaledb.com>
2016-06-21 18:38:10 +1000

    Update d_init_cas.sh

--- d_init_cas.sh
+++ d_init_cas.sh
@@ -5,4 +5,4 @@ sudo docker rm -f cas1
 #sudo docker rm -f cas2
 
 SID1=`sudo docker run -t -P  -d -i --name cas1  -p 4306   michaelfitz/myscaledb:106`
-SID2=`sudo docker run -t -P  -d -i --name cas2  -p 4306   michaelfitz/myscaledb:106`
+#SID2=`sudo docker run -t -P  -d -i --name cas2  -p 4306   michaelfitz/myscaledb:106`
